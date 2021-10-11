91d7dc288704210462d2b969859b373b24e5e377
Dragoshy <hedphug@gmail.com>
2021-10-11 08:37:26 +0300

    add document size item to StatusBar

--- src/features/statusbar/StatusBar.tsx
+++ src/features/statusbar/StatusBar.tsx
@@ -1,26 +1,41 @@
 import styled from '@emotion/styled'
 import React from 'react'
 import { useAppSelector } from '../../store/hooks'
-import { lastNotification } from '../editor/store/selectors'
+import {
+  documentContentSelector,
+  lastNotification,
+} from '../editor/store/selectors'
 
 const StyledStatusBar = styled.div`
   display: flex;
+  justify-content: end;
   align-items: center;
   padding: 0 1.5rem;
   min-height: 1.5rem;
   font-size: 0.75rem;
+  opacity: .7;
+`
 
-  /* hardcoded */
-  /* TODO go through a theme */
-  color: #1a1a1a75;
+const StatusBarItem = styled.div`
+  margin-right: 24px;
+  :last-of-type  {
+    margin-right: 0;
+  }
+  color: ${({ theme }) => theme.app.foregroundColor};
 `
 
 interface Props {}
 
 const StatusBar: React.FC<Props> = ({}: Props) => {
   const notification = useAppSelector(lastNotification)
+  const documentContent = useAppSelector(documentContentSelector)
 
-  return <StyledStatusBar>{notification.type}</StyledStatusBar>
+  return (
+    <StyledStatusBar>
+      <StatusBarItem>status: {notification.type}</StatusBarItem>
+      <StatusBarItem>length: {documentContent.length}</StatusBarItem>
+    </StyledStatusBar>
+  )
 }
 
 export default StatusBar
